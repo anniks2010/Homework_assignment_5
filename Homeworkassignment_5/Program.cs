c4f336643084e3ad30d17e6166e882f33733b89f
Anu <anniks_4@hotmail.com>
2019-10-28 21:54:21 +0200

    Array homework

---
+++ Homeworkassignment_5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Homeworkassignment_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         string userName;
+         string userNameReversed;
+            
+         Console.WriteLine("Enter your name:");
+         userName = Console.ReadLine().ToLower();
+
+         userNameReversed = ReverseString(userName);
+         
+         if (userName == userNameReversed)
+         {
+          Console.WriteLine($"Your name {userName} is palindrome string.");
+         }
+         else
+         {
+          Console.WriteLine($"Your name {userName} is not palindrome string.");
+         }
+            
+        } 
+        public static string ReverseString (string userName)
+        {
+            char[] charArray = userName.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+     
+    }
+}
